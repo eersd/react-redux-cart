cb4fbcca9a20182b1c0b2ab884e8c80f30a8ce58
eersada <internets.scholar@gmail.com>
2020-09-26 09:22:38 +0200

    orders processed and saved to DB, added Admin page that shows ordered list

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// import Pusher from "pusher";
+//import Pusher from "pusher";
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -7,60 +7,129 @@ const shortid = require("shortid");
 const app = express();
 app.use(bodyParser.json());
 
-const Pusher = require("pusher-js");
+const Pusher = require("pusher");
 const pusher = new Pusher({
-    appId: '1070545',
-    key: 'a692f130cf06402ee20f',
-    secret: '1e7009dead937e4b49b3',
-    cluster: 'eu',
-    encrypted: true
+  appId: "1070545",
+  key: "a692f130cf06402ee20f",
+  secret: "1e7009dead937e4b49b3",
+  cluster: "eu",
+  encrypted: true,
 });
 
-mongoose.connect("mongodb+srv://admin:eL1OHrceu7QjaTGp@cluster0.k4yoq.mongodb.net/shoppingCart?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://admin:nR1wfwxcI1b4s2FW@cluster0.ucyj4.mongodb.net/shoppingCart?retryWrites=true&w=majority"
+);
 
 const db = mongoose.connection;
 db.once("open", () => {
-    console.log("DB connected");
+  console.log("DB connected");
 
-    const listCollection = db.collection("productsList");
-    const changeStream = listCollection.watch();
+  const listCollection = db.collection("productsList");
+  const changeStream = listCollection.watch();
 
-    changeStream.on("change", (change) => {
-        console.log(change);
+  changeStream.on("change", (change) => {
+    console.log(change);
 
-        if(change.operationType === 'insert') {
-            const productDetails = change.fullDocument;
-            pusher.trigger('products', 'inserted',
-                {
-                    title: productDetails.title,
-                    description: productDetails.description,
-                    image: productDetails.image,
-                    price: productDetails.price,
-                    availableSizes: productDetails.availableSizes,
-                }
-            );
-        } else{
-            console.log('Error triggering Pusher');
-        }
-    });
+    if (change.operationType === "insert") {
+      const productDetails = change.fullDocument;
+      pusher.trigger("products", "inserted", {
+        title: productDetails.title,
+        description: productDetails.description,
+        image: productDetails.image,
+        price: productDetails.price,
+        availableSizes: productDetails.availableSizes,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+
+  const orderCollection = db.collection("orders");
+  const changeStreamOrder = orderCollection.watch();
+
+  changeStreamOrder.on("change", (change) => {
+    console.log(change);
+    if (change.operationType === "insert") {
+      const orderDetails = change.fullDocument;
+      pusher.trigger("orders", "inserted", {
+        email: orderDetails.email,
+        name: orderDetails.name,
+        address: orderDetails.address,
+        total: orderDetails.total,
+        cartItems: orderDetails.cartItems,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
 });
 
-
 const Product = mongoose.model(
-    "productsList",
-    new mongoose.Schema({
-        _id: {type: String, default: shortid.generate},
-        title: String,
-        description: String,
-        image: String,
-        price: Number,
-        availableSizes: [String],
-    })
+  "productsList",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+  })
 );
 
-app.get("/",(req,res) => {
-    res.status(200).send('hello world')
-})
+const Order = mongoose.model(
+  "orders",
+  new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      email: String,
+      name: String,
+      address: String,
+      total: Number,
+      cartItems: [
+        {
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number,
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+app.get("/api/orders", (req, res) => {
+  Order.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/api/orders", async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is required" });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello world");
+});
 
 // app.get("/api/products" , (req,res) => {
 //     const products = Product.find((err,data) => {
@@ -73,27 +142,27 @@ app.get("/",(req,res) => {
 //     });
 //     res.send(products);
 // });
-app.get("/api/products" , (req,res) => {
-    Product.find({},(err,data) => {
-        if (err){
-            res.status(500).send(err);
-        }else{
-            res.status(200).send(data);
-        }
-        console.log(err);
-    });
+app.get("/api/products", (req, res) => {
+  Product.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+    console.log(err);
+  });
 });
 
-app.post("/api/products", async (req,res) => {
-    const newProduct = new Product(req.body);
-    const savedProduct = await newProduct.save();
-    res.send(savedProduct);
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
 });
 
-app.delete("/api/products/:id", async(req,res) => {
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-    res.send(deletedProduct);
-})
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("serve at http://localhost:5000"));
+app.listen(port, () => console.log("serve at http://localhost:",port));
--- src/App.js
+++ src/App.js
@@ -1,126 +1,28 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Cart from "./components/Cart";
-import Filter from "./components/Filter";
-import Products from "./components/Products";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import AdminScreen from "./screens/AdminScreen";
+import HomeScreen from "./screens/HomeScreen";
 import store from "./store";
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="grid-container">
-          <header>
-            <a href="/">React Shopping Cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter></Filter>
-                <Products />
-                {/* <Products addToCart={this.addToCart}></Products> */}
-                {/* <Filter
-                  count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                /> */}
-
-                {/* <Products
-                  products={this.state.products}
-                  addToCart={this.addToCart}
-                /> */}
-              </div>
-              <div className="sidebar">
-                <Cart />
-                {/* <Cart
-                  createOrder={this.createOrder}
-                  cartItems={this.state.cartItems}
-                  removeFromCart={this.removeFromCart}
-                /> */}
-              </div>
-            </div>
-            Product List
-          </main>
-          <footer>All right is reserved</footer>
-        </div>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              <Link to="/">React Shopping Cart</Link>
+              <Link to="/admin">Admin</Link>
+            </header>
+            <main>
+              <Route path="/admin" component={AdminScreen} />
+              <Route path="/" component={HomeScreen} exact />
+            </main>
+            <footer>All right is reserved</footer>
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     products: data.products,
-  //     cartItems: localStorage.getItem("cartItems")
-  //       ? JSON.parse(localStorage.getItem("cartItems"))
-  //       : [],
-  //     size: "",
-  //     sort: "",
-  //   };
-  // }
-
-  // createOrder = (order) => {
-  //   alert("Implement order");
-  // };
-  // removeFromCart = (product) => {
-  //   const cartItems = this.state.cartItems.slice();
-  //   this.setState({
-  //     cartItems: cartItems.filter((item) => item._id !== product._id),
-  //   });
-  //   localStorage.setItem(
-  //     "cartItems",
-  //     JSON.stringify(cartItems.filter((item) => item._id !== product._id))
-  //   );
-  // };
-  // addToCart = (product) => {
-  //   const cartItems = this.state.cartItems.slice();
-  //   let alreadyInCart = false;
-  //   cartItems.forEach((item) => {
-  //     if (item._id === product._id) {
-  //       item.count++;
-  //       alreadyInCart = true;
-  //     }
-  //   });
-  //   if (!alreadyInCart) {
-  //     cartItems.push({ ...product, count: 1 });
-  //   }
-  //   this.setState({ cartItems });
-  //   localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  // };
-  // sortProducts = (event) => {
-  //   console.log(event.target.value);
-  //   const sort = event.target.value;
-  //   this.setState((state) => ({
-  //     sort: sort,
-  //     products: this.state.products
-  //       .slice()
-  //       .sort((a, b) =>
-  //         sort === "lowest"
-  //           ? a.price > b.price
-  //             ? 1
-  //             : -1
-  //           : sort === "highest"
-  //           ? a.price < b.price
-  //             ? 1
-  //             : -1
-  //           : a._id > b._id
-  //           ? 1
-  //           : -1
-  //       ),
-  //   }));
-  // };
-  // filterProducts = (event) => {
-  //   console.log(event.target.value);
-  //   if (event.target.value === "") {
-  //     this.setState({ size: event.target.value, product: data.products });
-  //   } else {
-  //     this.setState({
-  //       size: event.target.value,
-  //       products: data.products.filter(
-  //         (product) => product.availableSizes.indexOf(event.target.value) >= 0
-  //       ),
-  //     });
-  //   }
-  // };
 }
